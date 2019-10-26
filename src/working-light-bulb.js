cfbf3351edb94367bf37a0df516266bf92d077a3
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-10-26 13:10:53 -0700

    add working light bulb

---
+++ src/working-light-bulb.js
@@ -0,0 +1,30 @@
+/*
+Our stateless bulbs are a bit absurd, but they help us understand something
+about state, it's most useful when we can change it.
+
+"State" is the representation of a system at a particular point in time, in the
+case of the light bulb, our states are a "lit" state and an "unlit" state.
+
+Let's write a function that returns us a working light bulb.
+*/
+
+function workingLightBulb() {
+  let isLit = false
+
+  return {
+    isLit() {
+      return isLit
+    },
+    toggle() {
+      isLit = !isLit
+    },
+  }
+}
+
+const bulb = workingLightBulb()
+
+console.log(bulb.isLit())
+bulb.toggle()
+console.log(bulb.isLit())
+bulb.toggle()
+console.log(bulb.isLit())
