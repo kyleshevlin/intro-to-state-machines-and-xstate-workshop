ddf296a1ba277229d20fcf7a690e508c98b1c301
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-10-26 13:03:46 -0700

    add stateless bulbs

---
+++ src/stateless-light-bulb.js
@@ -0,0 +1,28 @@
+/*
+What if we didn't have "state", and things were just always what they were?
+That would be a bit of a strange world. Thinking about that too much might hurt
+your brain. But, let's consider an absurd example anyways, a light bulb.
+
+If we had a lamp in this stateless world, in order for the lamp to be on, we
+would give it a light bulb that was always lit, and when we needed it off,
+we would give it a lightbulb that was always unlit (or no bulb at all, but roll
+with me here).
+
+We can represent these stateless bulbs as functions that always return the same
+value
+*/
+
+function alwaysLitBulb() {
+  return { lit: true }
+}
+
+function alwaysUnlitBulb() {
+  return { lit: false }
+}
+
+function lamp(bulb) {
+  console.log(bulb.lit)
+}
+
+lamp(alwaysLitBulb())
+lamp(alwaysUnlitBulb())
