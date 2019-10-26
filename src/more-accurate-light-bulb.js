46142d051dc622863bb856a665d8bd0ddcfb8b9b
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-10-26 13:29:19 -0700

    add more accurate bulb

---
+++ src/more-accurate-light-bulb.js
@@ -0,0 +1,46 @@
+/*
+We made a working light bulb, but it's actually an inaccurate one. Light bulbs,
+at least up until recently, were pretty easy to break. Thus, we need a bulb
+that can be broken.
+
+Let's try and write a function that represents this.
+*/
+
+function moreAccurateBulb() {
+  let isLit = false
+  let isBroken = false
+
+  return {
+    isLit() {
+      return isLit
+    },
+    toggle() {
+      isLit = !isLit
+    },
+    isBroken() {
+      return isBroken
+    },
+    break() {
+      isBroken = !isBroken
+    },
+  }
+}
+
+const bulb = moreAccurateBulb()
+const logIsLit = () => {
+  console.log(`Bulb is lit: ${bulb.isLit()}`)
+}
+const logIsBroken = () => {
+  console.log(`Bulb is broken: ${bulb.isBroken()}`)
+}
+
+logIsLit()
+bulb.toggle()
+logIsLit()
+bulb.break()
+logIsBroken()
+logIsLit() // uh oh
+
+/*
+Uh oh! We've ended up in what should be an impossible state. How can a bulb both be lit and broken?
+*/
